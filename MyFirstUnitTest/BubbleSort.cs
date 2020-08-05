c7de860df21844db513abfc68400627cbf380bfa
Parterdev <litemeilateran@outlook.com>
2020-08-05 01:29:03 -0500

    Upload new implementations to sorting algorithms

--- MyFirstUnitTest/BubbleSort.cs
+++ MyFirstUnitTest/BubbleSort.cs
@@ -4,24 +4,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MyFirstUnitTest
 {
-    [TestClass]
-    public class BubbleSort
+  [TestClass]
+  public class BubbleSort
+  {
+
+    [TestMethod]
+    //Primer caso del ejercicio
+    public void BubbleSortNumbers()
     {
+      //Arrange
+      var elementos = new int[] { 1, -5, 4, 10, -9, 1, 0, 20, -8, 100, 1 };
+      var elementosEsperados = new int[] { -9, -8, -5, 0, 1, 1, 1, 4, 10, 20, 100 };
 
-        [TestMethod]
-        //Primer caso del ejercicio
-        public void BubbleSortNumbers()
-        {
-            //Arrange
-            var elementos = new int[] { 1, -5, 4, 10, -9, 1, 0, 20, -8, 100, 1 };
-            var elementosEsperados = new int[] { -9, -8, -5, 0, 1, 1, 1, 4, 10, 20, 100 };
+      //Act
+      var elementosOrdenados = Sorting.SelectionSort(elementos);
 
-            //Act
-            var elementosOrdenados = Sorting.SelectionSort(elementos);
-
-            //Assert
-            CollectionAssert.AreEqual(elementosOrdenados, elementosEsperados);
-        }
-      
+      //Assert
+      CollectionAssert.AreEqual(elementosOrdenados, elementosEsperados);
     }
+
+  }
 }
---
+++ MyFirstUnitTest/Sorting2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstUnitTest
+{
+    internal class Sorting2
+    {
+        public static int[] ShellSort(int[] elementos)
+        {
+
+            /**
+             * 1.- Definimos el dataset
+             * 2.- Creamos el valor del primer salto K = n/2
+             * 3.- Ejecutamos un loop para realizar los saltos y cambios hasta obtener el dataset ordenado
+             * **/
+
+            //Realizamos una asignacion de elementos a la variable array
+            int[] array = elementos;
+            int n = array.Length; //n sera igual a mi array de elementos
+            int salto = n / 2;
+            int aux;
+
+            //Ejecutamos el loop mientras salto sea mayor que 0
+            while (salto > 0)
+            {
+                //Primer ciclo
+                for (int i = salto; i < n; i++)
+                {
+                    int j = i - salto;
+                    //Entramos al ciclo interno
+                    while (j >= 0)
+                    {
+                        //Movemos los elementos
+                        int k = j + salto;
+                        if (array[j] < array[k])
+                        {
+                            j = -1; //Salimos del ciclo
+                        }
+                        else
+                        {
+                            aux = array[j];
+                            array[j] = array[k];
+                            array[k] = aux;
+                            j -= salto;
+                        }
+                    }
+                }
+                salto = salto / 2;
+            }
+            
+            return array;
+        }
+    }
+}
